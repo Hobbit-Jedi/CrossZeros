fa32ea9c66ddd58cc796a7986e9c6ac1126ffb9e
Hobbit Jedi <Hobbit_Jedi@mail.ru>
2017-03-26 14:10:05 +0300

    Создал новый проект.

---
+++ src/ua/net/hj/CrossZeros.java
@@ -0,0 +1,16 @@
+package ua.net.hj;
+
+/**
+ *
+ * @author Hobbit Jedi
+ */
+public class CrossZeros {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
